229bb2e612076fc0f41c004d104880b22dbd743d
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-17 23:46:17 -0500

    add dependencies, set up .env, start inquirer prompt and db connection

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: process.env.DB_PASSWORD,
+  database: 'employee_db'
+});
+
+module.exports = db;
