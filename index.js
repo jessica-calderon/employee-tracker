70f8ab554081cf536bf003c094f75f7634be3fc0
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-19 00:34:04 -0500

    add inquirer prompt switch logic

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cTable = require("console.table");
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
-questionPrompt();
+    questionPrompt();
 });
 function questionPrompt() {
     const firstQuestion = [{
@@ -20,16 +20,55 @@ function questionPrompt() {
     }]
 }
 inquirer.prompt(questionPrompt)
-.then(response => {
-    switch (response.choice) {
-        case "View all departments":
-            viewAll("DEPARTMENT");
-            break;
-        case "View all roles":
-            viewAll("ROLE");
-            break;
-        case "View all employees":
-        viewAll("EMPLOYEE");
-        break;
-    }
-})
+    .then(response => {
+        switch (response.choice) {
+            case "View all departments":
+                viewAll("DEPARTMENT");
+                break;
+            case "View all roles":
+                viewAll("ROLE");
+                break;
+            case "View all employees":
+                viewAll("EMPLOYEE");
+                break;
+            case "View employees by manager":
+                viewEmployeesByManager();
+                break;
+            case "View employees by department":
+                viewEmployeesByDept();
+                break;
+            case "Add a department":
+                addDept();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update role for an employee":
+                updateRole();
+                break;
+            case "Update employee's manager":
+                updateManager();
+                break;
+            case "Delete a department":
+                deleteDept();
+                break;
+            case "Delete a role":
+                deleteRole();
+                break;
+            case "Delete an employee":
+                deleteEmployee();
+                break;
+            case "View the total utilized budget of a department":
+                viewBudget();
+                break;
+            default:
+                connection.end();
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
